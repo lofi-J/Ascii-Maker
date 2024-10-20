c12f986f4780536a75e665f6fd0478cc4fa149f2
lofi <jsj2505@naver.com>
2024-10-20 16:44:30 +0900

    [add] 404 페이지 추가
     - BuildAsciiImage 경로 추가
     - BuildAsciiText 경로 추가

--- src/app/ascii/image/page.tsx
+++ src/app/ascii/image/page.tsx
@@ -1,10 +1,10 @@
 import styles from "./page.module.css";
 
 
-export default function Ascii() {
+export default function BuildAsciiImage() {
   return (
     <div className={styles.container}>
-    
+      image
     </div>
   );
 }
---
+++ src/app/ascii/text/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+
+export default function BuildAsciiText() {
+  return (
+    <div className={styles.container}>
+text
+    </div>
+  );
+}
