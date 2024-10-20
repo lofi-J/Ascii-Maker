6e2428abb34f13752912c8f4f65aff145eb3075c
lofi <jsj2505@naver.com>
2024-10-20 15:58:03 +0900

    [feat] 랜딩 페이지내 페이지 이동 기능
     - (w,s), (up, down), (j,k) 키로 모드 선택 가능 기능
    
     - Enter 또는 클릭으로 페이지 이동 기능

--- src/components/SelectMode/SelectMode.tsx
+++ src/components/SelectMode/SelectMode.tsx
@@ -1,20 +1,42 @@
 "use client";
 import styles from "./SelectMode.module.css";
 import Link from "next/link";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {useRouter} from "next/navigation";
 
 
 const SelectMode = () => {
   const [mode, setMode] = useState(1);
-  
-  const getClassName = (_mode: 0 | 1) => {
+  const router = useRouter();
+
+  const getClassName = (_mode: 1 | 2) => {
     return mode === _mode ? styles.active : '';
   }
-  
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const upKey = ['ArrowUp', 'w', 'k'];
+      const downKey = ['ArrowDown', 's', 'j'];
+
+      if (event.key === 'Enter') {
+        router.push(mode === 1 ? '/ascii/image' : '/ascii/text');
+      }
+
+      if (upKey.includes(event.key) && mode === 2) {
+        setMode(1);
+      } else if (downKey.includes(event.key) && mode === 1) {
+        setMode(2);
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [mode, router])
+
   return (
     <div className={styles.wrap}>
-      <Link className={`${styles.link} ${getClassName(1)}`} href={'/select/image'}>[1] Image to ASCII</Link>
-      <Link className={`${styles.link} ${getClassName(2)}`} href={'/select/text'}>[2] Text to ASCII</Link>
+      <Link className={`${styles.linkImage} ${getClassName(1)}`} href={'/ascii/image'}>[1] Image to ASCII</Link>
+      <Link className={`${styles.linkText} ${getClassName(2)}`} href={'/ascii/text'}>[2] Text to ASCII</Link>
     </div>
   );
 }
