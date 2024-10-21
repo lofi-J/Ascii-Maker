8ee34adbda3a4386428ffcdddd2c5a4b211aa0c8
lofi <jsj2505@naver.com>
2024-10-21 16:48:44 +0900

    [Refactor] 이미지 업로드 try catch
     - 추후 모달이 추가되면 모달을 통해 핸들링할 예정

--- src/components/ImageUploader/ImageUploader.tsx
+++ src/components/ImageUploader/ImageUploader.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import { MdOutlineUploadFile } from "react-icons/md";
 
 
-
 interface ImageUploader {
   imageFile: File | null;
   setIsUploadError: Dispatch<SetStateAction<boolean>>;
@@ -17,22 +16,41 @@ const ImageUploader = ({imageFile, setIsUploadError, setImageFile, onLoad}: Imag
   const inputRef = useRef<HTMLInputElement | null>(null);
   const [imageDataUrl, setImageDataUrl] = useState<string | null>(null); // 업로드한 이미지 data url
 
-  // TODO file reader error 핸들링 필요 (try, catch | reader.onerror)
   const handleImageUpload = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files === null) {
-      setIsUploadError(true);
-      return;
-    }
-    const file = event.target.files[0];
+    try {
+      if (event.target.files === null) {
+        setIsUploadError(true);
+        return;
+      }
+      const file = event.target.files[0];
 
-    if (file !== null) {
+      if (!file) {
+        console.error('has no file');
+        return;
+      }
+
+      // 부모 측 File 전달
       setImageFile(file);
+      // FileReader 생성
       const reader = new FileReader();
+
+      // 읽기 실패 시 에러 핸들링
+      reader.onerror = () => {
+        console.error('File reading has failed');
+        setIsUploadError(true);
+      }
+
+      // 파일 읽기
       reader.onloadend = () => {
         setImageDataUrl(reader.result as string);
       }
+      // DataURL 형식으로 읽기
       reader.readAsDataURL(file);
-    } else console.error('has no file');
+
+    } catch (error) {
+      console.error(error);
+      setIsUploadError(true);
+    }
   }
 
   // 렌더링 시간 계산
